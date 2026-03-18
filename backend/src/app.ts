137e11696b2b7f7ccb53da69db23d1dd6d1566f0
Jose Irola <joseangel0817@gmail.com>
2026-03-18 12:42:12 -0500

    hotfix: Env Failure

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -23,6 +23,6 @@ app.use('/api/tareas', tareasRoutes);
 app.use('/api/horarios', horariosRoutes);
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Servidor corriendo en puerto ${PORT}`);
+app.listen(PORT as number, '0.0.0.0', () => {
+  console.log(`Servidor corriendo en puerto ${PORT} (0.0.0.0)`);
 });
